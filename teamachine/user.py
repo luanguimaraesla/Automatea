d742cadf6e0095ee81d0c7d285f30f0bf780fe75
Luan Guimarães <luan@live.in>
2015-06-24 19:38:25 -0300

    Adding a tea manipulation methods to User class.

--- teamachine/user.py
+++ teamachine/user.py
@@ -1,6 +1,8 @@
 __author__ = 'luan'
 
-from .person import Person
+from person import Person
+from tea import Tea
+from teataste import TeaTaste
 
 class User(Person):
     def __init__(self, nick_name, password, name, mail, photo_path = None):
@@ -11,4 +13,18 @@ class User(Person):
         self.password = password
         self.preferred_tea_list = []
 
-    #def add_a_preferred_tea(self, ):
+    def add_a_preferred_tea(self, tea):
+        if not isinstance(tea, Tea):
+            raise IOError
+
+        self.preferred_tea_list.append(tea)
+
+    def create_tea(self, name, taste, water_ml = Tea._MID_WATER, sugar_wg = Tea._NO_SUGAR):
+        new_tea = Tea(name, taste, water_ml, sugar_wg)
+        return new_tea
+
+    def add_new_taste(self, name, manufacturer = None,
+                      origin = None, expiration_date = None,
+                      description = None, photo_path = None):
+        new_taste = TeaTaste(name, manufacturer, origin, expiration_date, description, photo_path)
+        return new_taste
