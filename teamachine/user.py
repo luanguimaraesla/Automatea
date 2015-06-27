f7861ca176052d859cbf1bb3c96f539b16c9fba5
Luan Guimarães <luan@live.in>
2015-06-26 22:35:30 -0300

    Adding some database interation methods to User class.

--- teamachine/user.py
+++ teamachine/user.py
@@ -3,6 +3,7 @@ __author__ = 'luan'
 from person import Person
 from tea import Tea
 from teataste import TeaTaste
+from models import UserDB, TeaDB, TeaTasteDB, Manufacturer
 
 class User(Person):
     def __init__(self, nick_name, password, name, mail, photo_path = None):
@@ -11,13 +12,13 @@ class User(Person):
         self.nick_name = nick_name
         #Lembrar de salgar a senha com Hash
         self.password = password
-        self.preferred_tea_list = []
+        self.my_tea_list = []
 
     def add_a_preferred_tea(self, tea):
         if not isinstance(tea, Tea):
             raise IOError
 
-        self.preferred_tea_list.append(tea)
+        self.my_tea_list.append(tea)
 
     def create_tea(self, name, taste, water_ml = Tea._MID_WATER, sugar_wg = Tea._NO_SUGAR):
         new_tea = Tea(name, taste, water_ml, sugar_wg)
@@ -26,5 +27,11 @@ class User(Person):
     def add_new_taste(self, name, manufacturer = None,
                       origin = None, expiration_date = None,
                       description = None, photo_path = None):
+
         new_taste = TeaTaste(name, manufacturer, origin, expiration_date, description, photo_path)
+        self.create_tea(name, new_taste)
+
         return new_taste
+
+
+    #def order(self, Tea):
