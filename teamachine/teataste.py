0f382cb06f1b7a822774cc915cd4211ca8a5df9f
Luan Guimarães <luan@live.in>
2015-07-06 15:15:57 -0300

    All classes were recreated to work @ beaglebone black.

---
+++ teamachine/teataste.py
@@ -0,0 +1,18 @@
+__author__ = 'luan'
+
+from models import Teatastedb, Manufacturerdb
+
+class TeaTaste:
+    def __init__(self, name, manufacturer = None, origin = None, expiration_date = None, description = None, photo_path = None):
+        self.name = name
+        self.manufacturer = manufacturer
+        self.origin = origin
+        self.expiration_date = expiration_date
+        self.description = description
+        self.photo_path = photo_path
+
+    def change_photo_path(self, new_photo_path):
+        if isinstance(new_photo_path, str):
+            self.photo_path = new_photo_path
+        else:
+            raise IOError
