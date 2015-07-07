0e3528667b922252edcc62bf7c42b1dc40dc7aa2
Luan Guimarães <luan@live.in>
2015-07-06 23:45:52 -0300

    Configuring models to work well @ beaglebone black

--- teamachine/models.py
+++ teamachine/models.py
@@ -12,15 +12,15 @@ _MID_WATER = 250  #ml
 _LOW_WATER = 100  #ml
 
 
-class Manufacturerdb(models.Model):
+class Manufacturer(models.Model):
     name = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.name
 
-class Teatastedb(models.Model):
+class Teataste(models.Model):
     name = models.CharField(max_length = 30)
-    manufacturer = models.ForeignKey(Manufacturerdb, null=True, blank=True)
+    manufacturer = models.ForeignKey(Manufacturer, null=True, blank=True)
     origin = models.CharField(max_length = 50)
     description = models.CharField(max_length = 500)
     photo_path = models.CharField(max_length = 50)
@@ -29,39 +29,39 @@ class Teatastedb(models.Model):
         return self.name
 
     def set_manufacturer(self, manufacturer):
-        manufacturer_list = Manufacturerdb.objects.all()
+        manufacturer_list = Manufacturer.objects.all()
         for each_manufacturer in manufacturer_list:
             if each_manufacturer.name == manufacturer:
-                each_manufacturer.teatastedb_set.add(self)
+                each_manufacturer.teataste_set.add(self)
                 break
         else:
-            newMf = Manufacturerdb(name = manufacturer)
+            newMf = Manufacturer(name = manufacturer)
             newMf.save()
-            newMf.teatastedb_set.add(self)
+            newMf.teataste_set.add(self)
             newMf.save()
 
-class Teadb(models.Model):
+class Tea(models.Model):
     name = models.CharField(max_length = 30)
     water_ml = models.IntegerField(default = _MID_WATER)
     sugar_wg = models.IntegerField(default = _NO_SUGAR)
-    taste_list = models.ManyToManyField(Teatastedb, null=True, blank=True)
+    taste_list = models.ManyToManyField(Teataste, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-class Userdb(models.Model):
+class User(models.Model):
     name = models.CharField(max_length = 50)
     mail = models.CharField(max_length = 30)
     photo_path = models.CharField(max_length = 50)
     nick_name = models.CharField(max_length = 20)
     password = models.CharField(max_length = 20)
-    my_tea_list = models.ManyToManyField(Teadb, null=True, blank=True)
+    my_tea_list = models.ManyToManyField(Tea, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
     def add_a_tea(self, tea):
-        if not isinstance(tea, Teadb):
+        if not isinstance(tea, Tea):
             raise IOError
 
         self.my_tea_list.add(tea)
@@ -69,7 +69,7 @@ class Userdb(models.Model):
     def create_tea(self, name, taste, water = 250, sugar = 0):
         newtea = self.my_tea_list.create(name = name, water_ml = water, sugar_wg = sugar)
         if taste:
-            if isinstance(taste, Teatastedb):
+            if isinstance(taste, Teataste):
                 self.my_tea_list.taste_list.add(taste)
             elif isinstance(taste, list):
                 for each_taste in taste:
@@ -82,7 +82,11 @@ class Userdb(models.Model):
                       origin = None,
                       description = None, photo_path = None):
 
-        self.save()
+        try:
+            User.objects.get(mail = self.mail)
+        except:
+            self.save()
+
         tea = self.create_tea(name = name, taste = None)
         teataste = tea.taste_list.create(name = name, origin = origin, description = description, photo_path = photo_path)
         teataste.set_manufacturer(manufacturer)
@@ -96,12 +100,6 @@ class Userdb(models.Model):
 
         self.save()
 
-    def get_user_by_id(self, user_id):
-        return Userdb.objects.get(pk = id)
+    # def order
 
-    def get_id_by_name(self, user_name):
-        return Userdb.objects.get(name = user_name)
-
-    def get_all_users(self):
-        return Userdb.objects.all()
 
