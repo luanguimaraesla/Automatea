d1ea99e6f1dca79feba6dab29b86cef8d22f1f0a
Luan Guimarães <luan@live.in>
2015-07-06 23:47:16 -0300

    adding some new URLS.

--- teamachine/urls.py
+++ teamachine/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^(?P<user_id>[0-9]+)/$', views.home, name='home'),
     url(r'^(?P<user_id>[0-9]+)/create/$', views.create, name='create'),
-    url(r'^(?P<user_id>[0-9]+)/order/$', views.order, name='order'),
+    url(r'^(?P<user_id>[0-9]+)/order/(?P<tea_id>[0-9]+)/$', views.order, name='order'),
     url(r'^(?P<user_id>[0-9]+)/manage/$', views.manage, name='manage'),
     url(r'^(?P<user_id>[0-9]+)/help/$', views.help, name='help'),
+    url(r'^singup/$', views.singup, name='signup'),
+    url(r'^register/$', views.register, name='register'),
 ]
