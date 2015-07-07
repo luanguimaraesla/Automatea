2b3d5d1842eed05aabd374490ebb05d316b9e8b5
Luan Guimarães <luan@live.in>
2015-07-06 23:44:17 -0300

    Configuring admin view.

--- teamachine/admin.py
+++ teamachine/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Userdb)
-admin.site.register(Teadb)
-admin.site.register(Teatastedb)
-admin.site.register(Manufacturerdb)
+admin.site.register(User)
+admin.site.register(Tea)
+admin.site.register(Teataste)
+admin.site.register(Manufacturer)
